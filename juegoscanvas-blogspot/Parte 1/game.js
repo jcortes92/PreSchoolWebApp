248e55d302fa8b612f313b4ae716998ae3c054b1
jcortes92 <jcortesanguita@gmail.com>
2014-04-02 19:56:15 +0200

    Juegos canvas. Parte 2

--- juegoscanvas-blogspot/Parte 1/game.js
+++ juegoscanvas-blogspot/Parte 1/game.js
@@ -1,10 +1,11 @@
 window.addEventListener('load',init,false);
 var canvas=null,ctx=null;
+var x=0, y=50;
 
 function init(){
     canvas=document.getElementById('canvas');
     ctx=canvas.getContext('2d');
-    paint(ctx);
+    run();
 }
 
 function paint(ctx){
@@ -14,4 +15,24 @@ function paint(ctx){
     ctx.strokeRect(250,0,50,50);
     ctx.strokeRect(250,100,50,50);
     ctx.strokeRect(0,100,50,50);
+    ctx.fillStyle='#FF0000';
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+    ctx.fillStyle='#000';
+    ctx.fillRect(x,y,10,10);
+}
+
+function run(){
+	requestAnimationFrame(run);
+	act();
+	paint(ctx);
+}
+
+function act(){
+	x+=8;
+	if(x>canvas.width)
+		x=0;
+	y+=32;
+	if(y>canvas.width)
+		y=0;
+	
 }
