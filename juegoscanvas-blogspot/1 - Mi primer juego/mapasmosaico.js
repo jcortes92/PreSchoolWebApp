faf0eb6f82eae58731f2a83fb13410e1e074da39
jcortes92 <jcortesanguita@gmail.com>
2014-04-14 16:07:21 +0200

    Limpieza código/comentarios.
    Siguiente: hacer horizontal

--- juegoscanvas-blogspot/1 - Mi primer juego/mapasmosaico.js
+++ juegoscanvas-blogspot/1 - Mi primer juego/mapasmosaico.js
@@ -18,137 +18,70 @@
     var line= [];
     var text= [];
 
-
-// //MAZE CODE
-//     function maze(x,y) {
-//     var n=x*y-1;
-//     if (n<0) {alert("illegal maze dimensions");return;}
-//     var horiz =[]; for (var j= 0; j<x+1; j++) horiz[j]= [];
-//        var verti =[]; for (var j= 0; j<y+1; j++) verti[j]= [];
-//         var here = [Math.floor(Math.random()*x), Math.floor(Math.random()*y)];
-//         var path = [here];
-//         var unvisited = [];
-//     for (var j = 0; j<x+2; j++) {
-//         unvisited[j] = [];
-//         for (var k= 0; k<y+1; k++)
-//             unvisited[j].push(j>0 && j<x+1 && k>0 && (j != here[0]+1 || k != here[1]+1));
-//     }
-//     while (0<n) {
-//         var potential = [[here[0]+1, here[1]], [here[0],here[1]+1],
-//             [here[0]-1, here[1]], [here[0],here[1]-1]];
-//         var neighbors = [];
-//         for (var j = 0; j < 4; j++)
-//             if (unvisited[potential[j][0]+1][potential[j][1]+1])
-//                 neighbors.push(potential[j]);
-//         if (neighbors.length) {
-//             n = n-1;
-//            var next= neighbors[Math.floor(Math.random()*neighbors.length)];
-//             unvisited[next[0]+1][next[1]+1]= false;
-//             if (next[0] == here[0])
-//                 horiz[next[0]][(next[1]+here[1]-1)/2]= true;
-//             else 
-//                 verti[(next[0]+here[0]-1)/2][next[1]]= true;
-//             path.push(here = next);
-//         } else 
-//             here = path.pop();
-//     }
-//     return {x: x, y: y, horiz: horiz, verti: verti};
-// }
- 
-// function display(m) {
-    
-//     for (var j= 0; j<m.x*2+1; j++) {
-        
-//         if (0 == j%2)
-//             for (var k=0; k<m.y*4+1; k++)
-//                 if (0 == k%4) 
-//                     line[k]= '+';
-//                 else
-//                     if (j>0 && m.verti[j/2-1][Math.floor(k/4)])
-//                         line[k]= ' ';
-//                     else
-//                         line[k]= '-';
-//         else
-//             for (var k=0; k<m.y*4+1; k++)
-//                 if (0 == k%4)
-//                     if (k>0 && m.horiz[(j-1)/2][k/4-1])
-//                         line[k]= ' ';
-//                     else
-//                         line[k]= '|';
-//                 else
-//                     line[k]= ' ';
-//         if (0 == j) line[1]= line[2]= line[3]= ' ';
-//         if (m.x*2-1 == j) line[4*m.y]= ' ';
-//         //line.push(''+'\r\n');
-//     }
-//     //return text.join('');
-//     return line
-// }
-// //END MAZE CODE
-
-function maze(x,y) {
-    var n=x*y-1;
-    if (n<0) {alert("illegal maze dimensions");return;}
-    var horiz =[]; for (var j= 0; j<x+1; j++) horiz[j]= [];
-    var  verti =[]; for (var j= 0; j<y+1; j++) verti[j]= [];
-        var here = [Math.floor(Math.random()*x), Math.floor(Math.random()*y)];
-        var path = [here];
-        var unvisited = [];
-        var next = [];
-    for (var j = 0; j<x+2; j++) {
-        unvisited[j] = [];
-        for (var k= 0; k<y+1; k++)
-            unvisited[j].push(j>0 && j<x+1 && k>0 && (j != here[0]+1 || k != here[1]+1));
+    //Creación laberinto
+    function maze(x,y) {
+        var n=x*y-1;
+        if (n<0) {alert("illegal maze dimensions");return;}
+        var horiz =[]; for (var j= 0; j<x+1; j++) horiz[j]= [];
+        var  verti =[]; for (var j= 0; j<y+1; j++) verti[j]= [];
+            var here = [Math.floor(Math.random()*x), Math.floor(Math.random()*y)];
+            var path = [here];
+            var unvisited = [];
+            var next = [];
+        for (var j = 0; j<x+2; j++) {
+            unvisited[j] = [];
+            for (var k= 0; k<y+1; k++)
+                unvisited[j].push(j>0 && j<x+1 && k>0 && (j != here[0]+1 || k != here[1]+1));
+        }
+        while (0<n) {
+            var potential = [[here[0]+1, here[1]], [here[0],here[1]+1],
+                [here[0]-1, here[1]], [here[0],here[1]-1]];
+            var neighbors = [];
+            for (var j = 0; j < 4; j++)
+                if (unvisited[potential[j][0]+1][potential[j][1]+1])
+                    neighbors.push(potential[j]);
+            if (neighbors.length) {
+                n = n-1;
+                next= neighbors[Math.floor(Math.random()*neighbors.length)];
+                unvisited[next[0]+1][next[1]+1]= false;
+                if (next[0] == here[0])
+                    horiz[next[0]][(next[1]+here[1]-1)/2]= true;
+                else 
+                    verti[(next[0]+here[0]-1)/2][next[1]]= true;
+                path.push(here = next);
+            } else 
+                here = path.pop();
+        }
+        return {x: x, y: y, horiz: horiz, verti: verti};
     }
-    while (0<n) {
-        var potential = [[here[0]+1, here[1]], [here[0],here[1]+1],
-            [here[0]-1, here[1]], [here[0],here[1]-1]];
-        var neighbors = [];
-        for (var j = 0; j < 4; j++)
-            if (unvisited[potential[j][0]+1][potential[j][1]+1])
-                neighbors.push(potential[j]);
-        if (neighbors.length) {
-            n = n-1;
-            next= neighbors[Math.floor(Math.random()*neighbors.length)];
-            unvisited[next[0]+1][next[1]+1]= false;
-            if (next[0] == here[0])
-                horiz[next[0]][(next[1]+here[1]-1)/2]= true;
-            else 
-                verti[(next[0]+here[0]-1)/2][next[1]]= true;
-            path.push(here = next);
-        } else 
-            here = path.pop();
-    }
-    return {x: x, y: y, horiz: horiz, verti: verti};
-}
- 
-function display(m) {
-    for (var j= 0; j<m.x*2+1; j++) {
-        
-        if (0 == j%2)
-            for (var k=0; k<m.y*4+1; k++)
-                if (0 == k%4) 
-                    line[k]= '+';
-                else
-                    if (j>0 && m.verti[j/2-1][Math.floor(k/4)])
-                        line[k]= ' ';
+     //Dibujar laberinto ASCII
+    function display(m) {
+        for (var j= 0; j<m.x*2+1; j++) {
+            
+            if (0 == j%2)
+                for (var k=0; k<m.y*4+1; k++)
+                    if (0 == k%4) 
+                        line[k]= '+';
                     else
-                        line[k]= '-';
-        else
-            for (var k=0; k<m.y*4+1; k++)
-                if (0 == k%4)
-                    if (k>0 && m.horiz[(j-1)/2][k/4-1])
-                        line[k]= ' ';
+                        if (j>0 && m.verti[j/2-1][Math.floor(k/4)])
+                            line[k]= ' ';
+                        else
+                            line[k]= '-';
+            else
+                for (var k=0; k<m.y*4+1; k++)
+                    if (0 == k%4)
+                        if (k>0 && m.horiz[(j-1)/2][k/4-1])
+                            line[k]= ' ';
+                        else
+                            line[k]= '|';
                     else
-                        line[k]= '|';
-                else
-                    line[k]= ' ';
-        if (0 == j) line[1]= line[2]= line[3]= ' ';
-        if (m.x*2-1 == j) line[4*m.y]= ' ';
-        text.push(line.join('')+'\r\n');
+                        line[k]= ' ';
+            if (0 == j) line[1]= line[2]= line[3]= ' ';
+            if (m.x*2-1 == j) line[4*m.y]= ' ';
+            text.push(line.join('')+'\r\n');
+        }
+        return text.join('');
     }
-    return text.join('');
-}
 
     function setMap(map,columns,blockSize){
         
@@ -160,13 +93,7 @@ function display(m) {
         for(var i=0;i<text.length;i++){
             for(var j=0; j<text[i].length;j++){
                 if((text[i][j]=='+')||(text[i][j]=='|')||(text[i][j]=='-'))
-                    wall.push(new Rectangle(col*blockSize,row*blockSize*2.5,blockSize*2,blockSize*2.5));
-                
-//                    if ((text[i][j]=='-') && (text[i][j+1]=='-') && (text[i][j+2]=='-')){
-//                    wall.push(new Rectangle(col*blockSize,row*blockSize,blockSize,blockSize));
-//                        wall.push(new Rectangle(col*blockSize,row*blockSize,blockSize,blockSize));
-//                        
-//                    }
+                    wall.push(new Rectangle(col*blockSize,row*blockSize*2.5,blockSize*2,blockSize*2.5));   
                 col++;
                 if(col>=columns){
                     row++;
